aef8a1543dff418afaaf955a59360da1c133b9ea
christopherkade <c.kade96@gmail.com>
2019-05-30 23:13:28 +0200

    🚀 Deploying to GH PAGES

--- src/hooks/useInputs.js
+++ src/hooks/useInputs.js
@@ -5,7 +5,7 @@ import { useState } from "react"
  */
 const useInputs = () => {
   const [bgColor, setBgColor] = useState("black")
-  const [title, setTitle] = useState("Hello DEV 🎉  ")
+  const [title, setTitle] = useState("Hello DEV 🎉")
   const [titleSize, setTitleSize] = useState("5")
   const [titleColor, setTitleColor] = useState("white")
   const [hasBorder, setHasBorder] = useState(true)
