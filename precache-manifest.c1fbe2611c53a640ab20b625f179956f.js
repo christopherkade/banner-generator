54a6b405f73f0d5197c9e2e0d1a5f428ecf1f709
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-03 21:01:23 +0000

    Deploy christopherkade/banner-generator to github.com/christopherkade/banner-generator.git:gh-pages

---
+++ precache-manifest.c1fbe2611c53a640ab20b625f179956f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251a40150d715b0fd5202e54b9a75dac",
+    "url": "/banner-generator/index.html"
+  },
+  {
+    "revision": "579fe36a1ccf4aac7983",
+    "url": "/banner-generator/static/css/main.5ba4ea57.chunk.css"
+  },
+  {
+    "revision": "0b99a9c76790f6a561f7",
+    "url": "/banner-generator/static/js/2.b7eb1bd4.chunk.js"
+  },
+  {
+    "revision": "579fe36a1ccf4aac7983",
+    "url": "/banner-generator/static/js/main.e1abb912.chunk.js"
+  },
+  {
+    "revision": "c5e5af4957a44f6c6dc6",
+    "url": "/banner-generator/static/js/runtime~main.8551277e.js"
+  }
+]);
